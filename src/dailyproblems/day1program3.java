2b17762f7c3e79503fbec26998e959fedcfddb15
Lijitha P <deepuliji7@gmail.com>
2026-07-16 11:26:24 +0530

    commit first

---
+++ src/dailyproblems/day1program3.java
@@ -0,0 +1,18 @@
+package dailyproblems;
+import java.util.*;
+public class day1program3 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
+            System.out.println("Integer Division = " + (a / b));
+            System.out.println("Floating Division = " + ((double) a / b));
+        }
+    }
+
+
