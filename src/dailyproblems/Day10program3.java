6abbc9e30914b8547f8021a836b68b7629ae7df0
Lijitha P <deepuliji7@gmail.com>
2026-07-16 10:32:50 +0530

    commit

---
+++ src/dailyproblems/Day10program3.java
@@ -0,0 +1,28 @@
+package dailyproblems;
+
+public class Day10program3 {
+
+    public int maxSubArray(int[] nums) {
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        Day10program3 obj = new Day10program3();
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println("Output: " + obj.maxSubArray(nums));
+    }
+}
